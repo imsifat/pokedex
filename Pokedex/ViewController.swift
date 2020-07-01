038c548d8f129644d52fee2ca9ca6fc527d4e2d9
Imran Sifat <imransifat@Imrans-MacBook-Pro.local>
2020-07-02 00:02:05 +0600

    first commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Imran Sifat on 1/7/20.
+//  Copyright © 2020 Imran Sifat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
